9bef83bd1067c2fb1d78c47bcff41201b664029b
matsu-o-0801 <over.cl.1210@gmail.com>
2025-03-02 09:22:48 +0900

    if文課題提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+let num =  Math.floor(Math.random()*15) + 1;
+
+if (num%3===0){
+  console.log ('３の倍数です');
+}
+
+else if (num%5===0){
+  console.log ('５の倍数です');
+}
+
+else if (num%3===0 && num%5===0){
+  console.log ('３と５の倍数です');
+}
+
+else {
+  console.log (num);
+}
