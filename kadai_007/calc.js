ee2f76785980ef0b0157027e06e18e34f196ee26
matsu-o-0801 <over.cl.1210@gmail.com>
2025-03-02 13:38:05 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,15 @@
 let num =  Math.floor(Math.random()*15)+ 1;
 
 if (num%3===0 && num%5===0){
-  console.log ('３と５の倍数です');
+  console.log ('3と5の倍数です');
 }
 
 else if (num%3===0){
-  console.log ('３の倍数です');
+  console.log ('3の倍数です');
 }
 
 else if (num%5===0){
-  console.log ('５の倍数です');
+  console.log ('5の倍数です');
 }
 
 else {
